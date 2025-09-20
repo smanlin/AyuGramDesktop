c2829957cf00a2903b46f70b75c536094725bff0
People-11 <41558215+People-11@users.noreply.github.com>
2025-09-21 02:28:43 +0930

    Build Windows Release
    Fix Link Problem
    Only Build Release To Speedup
    Add Cache
    
    Add cache
    
    (cherry picked from commit 9267b72d7d2680a7fbf166f4abd25313ccaae106)

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -962,6 +962,7 @@ win:
         -DCMAKE_DISABLE_FIND_PACKAGE_TIFF=TRUE ^
         -DCMAKE_DISABLE_FIND_PACKAGE_JPEG=TRUE ^
         -DCMAKE_DISABLE_FIND_PACKAGE_PNG=TRUE ^
+        -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=TRUE ^
         -DWITH_EXAMPLES=OFF
     cmake --build . --config Debug
     cmake --install . --config Debug
