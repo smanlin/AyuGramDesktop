25c4c654b1b5768d75008deb69393515414ce3f1
People-11 <41558215+People-11@users.noreply.github.com>
2025-12-05 02:48:08 +1030

    Fix selection cleared when shot cancelled

--- Telegram/SourceFiles/ayu/features/message_shot/message_shot.h
+++ Telegram/SourceFiles/ayu/features/message_shot/message_shot.h
@@ -45,6 +45,8 @@ void Make(not_null<QWidget*> box, const ShotConfig &config, const Fn<void(QImage
 
 void Show(ShotConfig config, Fn<void()> clearSelected);
 
+void Show(ShotConfig config, Fn<void()> clearSelected);
+
 void Wrapper(not_null<HistoryView::ListWidget*> widget, Fn<void()> clearSelected);
 void Wrapper(not_null<HistoryInner*> widget, Fn<void()> clearSelected);
 
