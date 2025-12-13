66c0841e8f2dfbf0e39539ef438a055e63398930
People-11 <41558215+People-11@users.noreply.github.com>
2025-12-13 23:56:30 +0800

    Make No Spoiler in Shot

--- Telegram/SourceFiles/history/view/history_view_element.cpp
+++ Telegram/SourceFiles/history/view/history_view_element.cpp
@@ -1314,6 +1314,15 @@ void Element::revealSpoilers() {
 	}
 }
 
+void Element::revealSpoilers() {
+	if (_text.hasSpoilers()) {
+		_text.setSpoilerRevealed(true, anim::type::instant);
+	}
+	if (_media) {
+		_media->revealSpoilers();
+	}
+}
+
 void Element::customEmojiRepaint() {
 	if (!(_flags & Flag::CustomEmojiRepainting)) {
 		_flags |= Flag::CustomEmojiRepainting;
